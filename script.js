0a1fd33951eca99606f0752b9d93255017a3d3d7
David Martin <davidmartinyusufu@gmail.com>
2023-09-12 10:11:38 +0000

    Add new feature: Theme toggle functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import "./styles.css"
-
+// Get references to HTML elements
 const themeToggle = document.getElementById('themeToggle');
 const equal = document.querySelector('#equal');
 const form = document.querySelector('form');
@@ -9,9 +8,36 @@ const resultEL = document.querySelector('.result');
 const body = document.body;
 const root = document.documentElement;
 
-let result = '';
-let current = '';
-let inputDisplay = '';
-let evaluation = false;
-let basicOperations = { '+': '+', '/': '/', '*': '*', '-': '-' };
+// Initialize variables
+let result = ''; // Stores the current mathematical expression
+let current = ''; // Stores the current user input
+let inputDisplay = ''; // Displays the user input and result
+let evaluation = false; // Flag to indicate if an evaluation is in progress
+let basicOperations = { '+': '+', '/': '/', '*': '*', '-': '-' }; // Basic arithmetic operations
 
+// Set a CSS variable to match the width of the result element
+root.style.setProperty('--width-to-match', `${resultEL.clientWidth}px`);
+
+// Theme toggle functionality
+const theme = (theme, inverse = theme === 'dark' ? 'light' : 'dark') => {
+  themeToggle.checked = theme === 'dark' ? true : false;
+  body.classList.remove(`${inverse}-theme`);
+  body.classList.add(`${theme}-theme`);
+  localStorage.setItem('theme', `${theme}-theme`);
+};
+
+function themeSwitch() {
+  if (this.checked === true) theme('dark');
+  else theme('light');
+}
+
+themeToggle.addEventListener('click', themeSwitch);
+
+// Retrieve saved theme from local storage
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+  body.classList.add(savedTheme);
+  if (savedTheme === 'dark-theme') theme('dark');
+  else theme('light');
+}
